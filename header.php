bd096838046bb7d132d9d7f6e9a97be4fe979835
Dinesh kumar <dkdon812002@gmail.com>
2023-03-16 22:21:17 +0530

    16_03_2023

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <?php
- include('login_validate.php');
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,6 +9,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>header</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <style>
+        body{
+          overflow-x:hidden;
+        }
+        .user_logo{
+          cursor: pointer;
+        }
+        .email{
+          cursor:not-allowed;
+          user-select:none;
+        }
+      </style>
   
 </head>
 <body>
@@ -19,18 +31,30 @@
       <img class="logo" src="images/favicon.ico" alt="" width="40" height="40">
     </a>
 <a class="navbar-brand text-decoration-none text-white" href="#">e-com site</a>
+<a class="navbar-brand" href="cart_info.php">
+      <img class="logo ms-2 p-1 bg-white rounded-circle pe-2" src="images\icons8-shopping-cart-50.png" alt="cart_image" width="40" height="40">
+       </a>
 
 </div>
 <div class="links">
     <ul class="list-unstyled d-flex px-3 ">
+    
     <?php 
-    if($_SESSION["username"]==""){
-     echo '<a class="user_logo ms-2 px-3 py-1 bg-success rounded text-decoration-none text-white" href="login.php">'. "login ".'</a>';
+    if(!isset($_SESSION['username'])){
+     echo '<a class="user_logo ms-2 px-3 py-1 bg-success rounded text-decoration-none text-white" href="login.php">'. "login".'</a>';
       }
       else{
-        echo '<p class="text text-white me-3">'.'welcome'.'</p>'.'<a class="user_logo ms-2 p-2 py-0 fs-4 bg-success rounded-circle text-decoration-none text-white" href="user_info.php">'. strtoupper($_SESSION["username"][0]) .'</a>'  ; 
-      }?>
-     
+        echo '<p class="text text-white me-3">'.'welcome'.'</p>'.'<a class="user_logo ms-2 p-2 py-0 fs-4 bg-success rounded-circle text-decoration-none text-white" onclick="user_info()">'. strtoupper($_SESSION["username"][0]) .'</a>'  ;
+        echo '<form class="d-flex" action="logout.php" method="POST">';
+        echo '<input type="submit" name="submit" value="logout" class="bg-success rounded mx-3">'; 
+        echo '</form>';
+      }
+        
+        ?>
+        
+        
+      
+      
       <li class="text text-white px-3 me-4"><a href="#product_link" class="link text-white text-decoration-none">shop</a></li>
       <li class="text text-white px-3 me-4"><a href="aboutus2.php" class="link text-white text-decoration-none">Aboutus</a></li>
       <!-- <li class="text text-white px-3"><button class="btn btn-danger"><a href="logout.php" class="text-decoration-none text-white">logout</a></button></li> -->
@@ -42,4 +66,11 @@
 </body>
 
   <script src="js/bootstrap.min.js"></script>
+  <script>
+    function user_info(){
+      window.alert("<?php echo 'logged in as  '. $_SESSION["username"]; ?>");
+    
+    }
+
+  </script>
 </html>
